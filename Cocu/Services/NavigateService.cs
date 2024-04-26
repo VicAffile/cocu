eca2a3db29f3d353f1e44da351852e359761ab7e
Victor Affilé <60343102+VicAffile@users.noreply.github.com>
2024-04-26 23:02:52 +0200

    feat-02 : Ajout de la navigation (#4)

---
+++ Cocu/Services/NavigateService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Cocu.Services
+{
+    internal class NavigateService
+    {
+        private static Frame _frame;
+
+        public static void Initialize(Frame frame)
+        {
+            _frame = frame;
+        }
+    }
+}
